1463a956bd8d30adf5b7d5f9699187282e7a0593
ilevd <ilevd@mail.ru>
2016-09-30 14:38:05 +0700

    Update route, use anychart.fromJson

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,16 @@ var Fruit = require('../models/Fruit.js');
 router.get('/', function(req, res, next) {
   Fruit.find({}).select('name value -_id').sort({'value': -1}).limit(5).exec(function (err, fruits) {
     if (err) return next(err);
-    res.render('index', {title: 'Anychart NodeJS demo',
-      chartTitle: "Top 5 fruits",
-      chartData: JSON.stringify(fruits)
-    });
+      // chart JSON data
+      var json = {
+          chart: {
+              type: "pie",
+              title: 'Top 5 Fruits',
+              data: fruits,
+              container: "container"
+          }
+      };
+    res.render('index', {title: 'Anychart NodeJS demo', chartData: JSON.stringify(json)});
   });
 });
 
