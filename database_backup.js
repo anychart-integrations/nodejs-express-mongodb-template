82c82e434d1d734ab7fe1105bd14293ba6f2264e
ilevd <ilevd@mail.ru>
2016-10-11 15:58:49 +0700

    Update database_backup.js

--- database_backup.js
+++ database_backup.js
@@ -1,4 +1,5 @@
 use anychart_db
+db.fruits.drop();
 db.fruits.insert({"name":"Apple", "value": 10})
 db.fruits.insert({"name":"Orange", "value": 20})
 db.fruits.insert({"name":"Banana", "value": 5})
